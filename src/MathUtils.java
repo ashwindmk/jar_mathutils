1cd622fb0418c30e7c69bc8dc797fb3f622f5fb0
Ashwin Dinesh <ashwin.dinesh01@gmail.com>
2019-11-02 21:31:28 +0530

    Deploy initial code

---
+++ src/MathUtils.java
@@ -0,0 +1,35 @@
+public class MathUtils {
+    public static void main(String[] args) {
+        if (args == null || args.length == 0) {
+            System.out.println("No arguments passed");
+            System.out.println("Usage: add 12.5 2.5");
+            return;
+        }
+
+        String op = args[0];
+
+        if (op.equals("add")) {
+            double a = Double.valueOf(args[1]);
+            double b = Double.valueOf(args[2]);
+            add(a, b);
+        }
+
+        else if (op.equals("sub")) {
+            double a = Double.valueOf(args[1]);
+            double b = Double.valueOf(args[2]);
+            sub(a, b);
+        }
+
+        else {
+            System.out.println("Invalid operation: " + op);
+        }
+    }
+
+    private static void add(double a, double b) {
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+
+    private static void sub(double a, double b) {
+        System.out.println(a + " - " + b + " = " + (a - b));
+    }
+}
